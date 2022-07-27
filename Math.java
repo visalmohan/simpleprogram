6bdc1a3118383ca1aee68c2c9314f802d2b87fdf
Visal <visalmohanlal2591814@gmail.com>
2022-07-27 11:11:00 +0530

    mathprogram

---
+++ Math.java
@@ -0,0 +1,13 @@
+class Math {
+public static void main (String args[]) {
+int a=7;
+int b=4;
+int c;
+int d;
+c=a+b;
+d=a-b;
+System.out.println(a++);
+System.out.println(b++);
+System.out.println("the value is "+c);
+System.out.println("the value is "+d);
+}}

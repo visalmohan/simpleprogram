3b2ae956c7eddeca21b9a62ba5af471719bdd08a
visalmohan <109943319+visalmohan@users.noreply.github.com>
2022-07-27 11:13:03 +0530

    added multiplication

--- Math.java
+++ Math.java
@@ -4,10 +4,13 @@ int a=7;
 int b=4;
 int c;
 int d;
+  int e;
 c=a+b;
 d=a-b;
+  e=a*b;
 System.out.println(a++);
 System.out.println(b++);
 System.out.println("the value is "+c);
-System.out.println("the value is "+d);
+System.out.println("the value is "+d)
+  Sytem.out.println("the value is "+e);
 }}
